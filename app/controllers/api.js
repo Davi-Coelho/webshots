18383f3c3bfcbb721b37612ca1f93110d4ba4006
Davi Coelho <davi.cmbarreto@gmail.com>
2023-01-22 16:40:39 -0400

    captureWebsite testado;

--- app/controllers/api.js
+++ app/controllers/api.js
@@ -1,9 +1,12 @@
 import captureWebsite from 'capture-website'
 
 export async function getWebshots (application, req, res) {
-    const base64 = await captureWebsite.base64('https://www.google.com')
-    console.log(base64)
-    res.send(base64)
+    const base64 = await captureWebsite.base64('https://ongaku.davicoelho.com', {
+        type: "png",
+        scaleFactor: 0.2
+    })
+    // console.log(base64)
+    res.send(`<img src='data:image/png;base64,${base64}'  alt="site"/>`)
 }
 
 export async function insertWebshot (application, req, res) {
